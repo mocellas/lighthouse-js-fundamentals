1c76fcc3bd8f857b73e408c219ac6d37018c21ea
mocellas <fernando.mocellin@gmail.com>
2017-02-28 13:56:35 +0000

    Adds conditionals exercises

---
+++ go-outside.js
@@ -0,0 +1,11 @@
+//var raining = true;
+var cold = false;
+
+//if (raining){
+//	console.log("Don`t forget your umbrella");
+}
+if (cold){
+	console.log("Make sure you pick out a scarf");
+}else{
+console.log("Now you are ready to go outside");
+}
